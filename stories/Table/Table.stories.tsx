f0efc0271f6b00a477154e5aee7f4422efa171c2
Sefirosweb <sefirosweb@gmail.com>
2023-09-26 22:40:07 +0200

    Add all types

--- stories/Table/Table.stories.tsx
+++ stories/Table/Table.stories.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { Table, TableRef } from '../../src';
-import { columns, crudOptions, onSubmitFn, useQueryOptionsLazy } from './tableProps';
+import { columns, crudOptions, onSubmitFn, useQueryOptionsEagle, useQueryOptionsLazy } from './tableProps';
 
 const meta: Meta = {
   title: 'Tables/Table',
@@ -12,7 +12,7 @@ export default meta;
 
 type Story = StoryObj<typeof Table>;
 
-// const crudOptions2 = { ...crudOptions, enableGlobalFilterLabels: undefined }
+const crudOptions2 = { ...crudOptions, enableGlobalFilterLabels: undefined }
 
 export const LazyTemplate: Story = {
   args: {
@@ -41,35 +41,83 @@ export const LazyTemplate: Story = {
   }
 }
 
-// export const LazyTemplateGlobal: Story = {
-//   args: {
-//     tableProps: {
-//       useQueryOptions: useQueryOptionsLazy,
-//       columns,
-//       crudOptions: crudOptions2,
-//       lazy: true
-//     }
-//   },
-// }
+export const LazyTemplateGlobal: Story = {
+  args: {
+    tableProps: {
+      useQueryOptions: useQueryOptionsLazy,
+      columns,
+      crudOptions: crudOptions2,
+      lazy: true
+    }
+  },
+  render: (props) => {
+    const tableRef = useRef<TableRef>(null)
+    crudOptions2.onSubmitFn = (data, action) => onSubmitFn(data, action, tableRef.current)
 
-// export const EagleTemplate: Story = {
-//   args: {
-//     tableProps: {
-//       useQueryOptions: useQueryOptionsEagle,
-//       columns,
-//       crudOptions,
-//       lazy: false
-//     }
-//   },
-// }
+    return (
+      <div>
+        <div>
+          This is Lazy load template
+        </div>
+        <div className='mt-3'>
+          <Table {...props} ref={tableRef} />
+        </div>
+      </div>
+    )
 
-// export const EagleTemplateGlobal: Story = {
-//   args: {
-//     tableProps: {
-//       useQueryOptions: useQueryOptionsEagle,
-//       columns,
-//       crudOptions: crudOptions2,
-//       lazy: false
-//     }
-//   },
-// }
+  }
+}
+
+export const EagleTemplate: Story = {
+  args: {
+    tableProps: {
+      useQueryOptions: useQueryOptionsEagle,
+      columns,
+      crudOptions,
+      lazy: false
+    }
+  },
+  render: (props) => {
+    const tableRef = useRef<TableRef>(null)
+    crudOptions.onSubmitFn = (data, action) => onSubmitFn(data, action, tableRef.current)
+
+    return (
+      <div>
+        <div>
+          This is Lazy load template
+        </div>
+        <div className='mt-3'>
+          <Table {...props} ref={tableRef} />
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export const EagleTemplateGlobal: Story = {
+  args: {
+    tableProps: {
+      useQueryOptions: useQueryOptionsEagle,
+      columns,
+      crudOptions: crudOptions2,
+      lazy: false
+    }
+  },
+  render: (props) => {
+    const tableRef = useRef<TableRef>(null)
+    crudOptions2.onSubmitFn = (data, action) => onSubmitFn(data, action, tableRef.current)
+
+    return (
+      <div>
+        <div>
+          This is Lazy load template
+        </div>
+        <div className='mt-3'>
+          <Table {...props} ref={tableRef} />
+        </div>
+      </div>
+    )
+
+  }
+}
