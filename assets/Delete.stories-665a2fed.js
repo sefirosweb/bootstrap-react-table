14d4576a49696de728090ef18faa0c9e7f834bc1
sefirosweb <20754836+sefirosweb@users.noreply.github.com>
2023-11-20 11:12:13 +0000

    Deploying to gh-pages from @ sefirosweb/bootstrap-react-table@24aecac74ad212d2bddaf6ef3808134a2ab0cf4e 🚀

---
+++ assets/Delete.stories-665a2fed.js
@@ -0,0 +1,10 @@
+import{b as d}from"./index-f582422f.js";import"./index-f1286426.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";import"./inheritsLoose-843c0be4.js";const b={title:"Components/Buttons/Delete",component:d},e={args:{disabled:!1}},r={args:{disabled:!0}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    disabled: false
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var o,n,c;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const g=["Active","Disabled"];export{e as Active,r as Disabled,g as __namedExportsOrder,b as default};
+//# sourceMappingURL=Delete.stories-665a2fed.js.map
---
+++ assets/Edit.stories-24704689.js
@@ -0,0 +1,10 @@
+import{E as i}from"./index-f582422f.js";import"./index-f1286426.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";import"./inheritsLoose-843c0be4.js";const b={title:"Components/Buttons/Edit",component:i},e={args:{disabled:!1}},r={args:{disabled:!0}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    disabled: false
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var o,d,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(n=(d=r.parameters)==null?void 0:d.docs)==null?void 0:n.source}}};const g=["Active","Disabled"];export{e as Active,r as Disabled,g as __namedExportsOrder,b as default};
+//# sourceMappingURL=Edit.stories-24704689.js.map
