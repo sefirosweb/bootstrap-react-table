3d809e3955107933fdb166da2f2981245d249f2e
sefirosweb <20754836+sefirosweb@users.noreply.github.com>
2024-05-04 07:37:13 +0000

    Deploying to gh-pages from @ sefirosweb/bootstrap-react-table@4584301bc97ca1c3a215523fd5249cd9e31c9f6f 🚀

---
+++ assets/Basic.stories-1MIQYT5k.js
@@ -0,0 +1,5 @@
+import{T as o}from"./index-H3FBRpbc.js";import{u as t}from"./tableProps-Bcq9LBIs.js";import"./index-Dl6G-zuu.js";import"./index-Beu9b4Vr.js";import"./inheritsLoose-BSOQp05f.js";import"./index-D1_ZHIBm.js";const d={title:"Tables/Basic",component:o},c={useQueryOptions:t,columns:[{accessorKey:"uuid",header:"UUID"},{accessorKey:"name",header:"Name",enableSorting:!0},{accessorKey:"description",header:"Desc."},{accessorKey:"price",header:"€",enableSorting:!0}],crudOptions:{primaryKey:"uuid"},lazy:!1},e={args:{tableProps:c}};var r,a,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    tableProps: tableProps
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const y=["BasicTemplate"];export{e as BasicTemplate,y as __namedExportsOrder,d as default};
--- assets/Refresh.stories-BoUnTPtu.js
+++ assets/Refresh.stories-BoUnTPtu.js
@@ -1,4 +1,4 @@
-import{R as d}from"./index-CJMv-7GL.js";import"./index-Dl6G-zuu.js";import"./index-Beu9b4Vr.js";import"./inheritsLoose-BSOQp05f.js";import"./index-D1_ZHIBm.js";const b={title:"Components/Buttons/Refresh",component:d},e={args:{disabled:!1}},r={args:{disabled:!0}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+import{R as d}from"./index-H3FBRpbc.js";import"./index-Dl6G-zuu.js";import"./index-Beu9b4Vr.js";import"./inheritsLoose-BSOQp05f.js";import"./index-D1_ZHIBm.js";const b={title:"Components/Buttons/Refresh",component:d},e={args:{disabled:!1}},r={args:{disabled:!0}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
   args: {
     disabled: false
   }
